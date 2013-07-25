19acf9bdffc00fb5398218b955ff378af1758cf6
Chuanpin Zhu <zcppku@gmail.com>
2013-07-24 18:07:59 -0700

    update some migrations

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,7 @@
 class Asset < ActiveRecord::Base
-  default_scope  { where("attachment_file_name IS NULL")}
+  default_scope  { where("attachment_file_name NOT NULL")}
   
   belongs_to :attachable, :polymorphic => true
+  
+  validates_attachment_size   :attachment, :less_than => 10.megabytes
 end
