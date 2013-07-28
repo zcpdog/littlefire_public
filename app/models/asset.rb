e47d8759caa05308fdffbb62d8d0fbfd46e4c12b
Chuanpin Zhu <zcppku@gmail.com>
2013-07-28 16:46:05 -0700

    delete some deprecated file

--- app/models/asset.rb
+++
@@ -1,7 +0,0 @@
-class Asset < ActiveRecord::Base
-  default_scope  { where("attachment_file_name NOT NULL")}
-  
-  belongs_to :attachable, :polymorphic => true
-  
-  validates_attachment_size   :attachment, :less_than => 10.megabytes
-end
