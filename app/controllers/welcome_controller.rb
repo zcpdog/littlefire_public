d35054ebb1afd9edf9c46c7a2fbcd2698407da7c
Chuanpin Zhu <zcppku@gmail.com>
2013-08-23 23:57:56 -0700

    bug fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @deals = Deal.order('id DESC').page params[:page]
-    @favorited_list = cookies[:favorited_list].split(",") || []
     respond_to do |wants|
       wants.html # index.html.erb
     end
