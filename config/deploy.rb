56bcfc2e44d8d1cf928d1e21853c4390c01f0dac
zcpdog <zcppku@gmail.com>
2014-03-18 17:57:06 -0700

    update repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :stages, ["staging", "production"]
 set :default_stage, "staging"
 default_run_options[:pty] = true
 set :application, "littlefire"
-set :repository,  "git@bitbucket.org:zcpdog/littlefire.git"
+set :repository,  "git@github.com:zcpdog/littlefire.git"
 set :deploy_via, :remote_cache
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
