2a640586299dcef9e6472c07721fba25db6665c8
zcpdog <zcppku@gmail.com>
2014-03-21 23:15:03 -0700

    disable cache in show pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Littlefire::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, "localhost", {:namespace =>"maishoudang", :compress => true }
+  config.cache_store = :dalli_store, "localhost", {namespace: "maishoudang", expires_in: 1.day, compress: true }
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
