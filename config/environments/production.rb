4c2b64c546da75c96b3a9ee31c4cc3696b89f045
zcpdog <zcppku@gmail.com>
2014-03-21 22:21:05 -0700

    remove expires_in for cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Littlefire::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, "localhost", {:namespace =>"maishoudang", :expires_in => 12.hours, :compress => true }
+  config.cache_store = :dalli_store, "localhost", {:namespace =>"maishoudang", :compress => true }
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
